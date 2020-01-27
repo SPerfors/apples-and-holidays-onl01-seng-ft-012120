66d336ddfe92334b75e999fe55cc649c79fc4201
Steven Perfors <stevecanbuild@gmail.com>
2020-01-27 19:57:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, hash|
-   puts "#{season.capitalize}"
+   puts "#{season.capitalize}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
